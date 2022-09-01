8cd7ddc75e957cc90207784947833cf8919dffdf
João paulo Garcia <jpdgarcia@gmail.com>
2022-09-01 16:30:38 -0300

    Arredondando os segundo

--- Q18.py
+++ Q18.py
@@ -1,6 +1,9 @@
 #Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
 
+import math
+
+
 print("Vamos calcular o tempo de download do seu aquivo ")
 downloadStr = input("Qual o tamanho do aquivo que você pretende baixar (em MB)?")
 velocidadeStr = input("Qual a sua velocidade de download (em mbps)?")
@@ -12,4 +15,4 @@ tempoSeg = download/velocidade
 tempoMin = tempoSeg//60
 restoSeg = tempoSeg%60
 
-print("o tempo aproximado de download desse aquivo é:", tempoMin,"minutos e",restoSeg,"segundo")
+print("o tempo aproximado de download desse aquivo é:", tempoMin,"minutos e",math.ceil(restoSeg),"segundo")
