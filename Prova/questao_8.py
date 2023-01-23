36108b52868575d7120f9bb0701e5ce94569c3f6
Joao Paulo Garcia <joao.garcia@luizalabs.com>
2023-01-23 10:03:30 -0300

    feat: questao 8 resolvendo bugs

--- Prova/questao_8.py
+++ Prova/questao_8.py
@@ -3,36 +3,44 @@
 # Para o programa teremos as seguinte chamadas:
 
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-contador = {"numero": 0}
+
+with open("contador.json", "r") as json_file:
+    contador = json.load(json_file)
+
 
 @app.route('/contador', methods=['POST'])
-def contador(contador):
+def contador():
+    global contador
     arg = request.get_json()
     numero = arg["numero"]
     contador = numero
-    return jsonify(contador)
+    return jsonify(contador), 201
 
 @app.route('/contador', methods=['GET'])
-def mostra(contador):
-    return jsonify(contador)
+def mostra():
+    global contador
+    return jsonify(contador), 200
 
 @app.route('/contador/incrementa', methods=['PUT'])
-def incrementa(contador):
+def incrementa():
+    global contador
     arg = request.get_json()
     numero = arg["numero"]
     contador = contador + numero
     arg = request.get_json()
-    return jsonify(contador)
+    return jsonify(contador), 202
 
 @app.route('/contador', methods=['DELETE'])
-def delete(contador):
+def delete():
+    global contador
     contador = 0
     return contador
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True), 202
